eeb4df500c46a800c60cc6057637cbb8fbcf6937
favour orji <favour@learnflo.co>
2023-06-03 13:38:11 +0100

    finished this project

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Transaction } from './Transaction';
+import { GlobalContext } from '../context/GlobalState'
 
 export const TransactionList = () => {
+    const {transactions} = useContext(GlobalContext);
+
+    // console.log(context)
+
   return (
     <>
         <h3>History</h3>
-        <ul id='list' className='list'>
-            <li className='minus'>
-                Cash<span>-$400</span><button className='delete-btn'>x</button>
-            </li>
+        <ul className='list'>
+            {transactions.map(transaction => (
+              <Transaction key={transaction.id} transaction= {transaction} />  
+            ))}
+            
         </ul>
     </>
   )
